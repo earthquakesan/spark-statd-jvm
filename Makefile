b677617189203b5908b076a7a8db96acba0cbc29
Ivan Ermilov <earthquakesan@gmail.com>
2018-01-28 18:51:43 +0100

    setup which works with YARN

--- Makefile
+++ Makefile
@@ -7,22 +7,23 @@ start-profiler-bash: download build
 start-cluster: download
 	docker-compose up -d
 
+start-profiler-yarn:
+	docker-compose -f docker-compose.profiler.yarn.yml up
+
+start-cluster-yarn:
+	docker-compose -f docker-compose.yarn.yml up -d
+
 network:
 	docker network create spark-profiling
 
 build:
 	docker build -t spark-profiler:latest ./profiler
 
-download: download-compose download-env-file download-example-app
+download: download-compose download-example-app
 
 download-compose:
 	if [ ! -f docker-compose.yml ]; then \
 	wget -O docker-compose.yml https://raw.githubusercontent.com/big-data-europe/docker-spark/1.6.3-nohadoop-java8/docker-compose.yml ; \
-	fi
-
-download-env-file:
-	if [ ! -f hadoop-hive.env ]; then \
-	wget -O hadoop-hive.env https://raw.githubusercontent.com/big-data-europe/docker-spark/1.6.3-nohadoop-java8/hadoop-hive.env ; \
 	fi
 
 download-example-app:
--- profiler/submit.sh
+++ profiler/submit.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-export SPARK_MASTER_URL=spark://${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}
+[ -z $SPARK_MASTER_URL ] && { export SPARK_MASTER_URL=spark://${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}; }
 
 echo "Submit application ${SPARK_APPLICATION_JAR_LOCATION} with main class ${SPARK_APPLICATION_MAIN_CLASS} to Spark master ${SPARK_MASTER_URL}"
+echo "Additional Spark configuration params: ${SPARK_ADDITIONAL_CONF}"
 echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
 
 /spark/bin/spark-submit \
-    --jars /statsd-jvm-profiler.jar \
-    --conf spark.executor.extraJavaOptions=-javaagent:"/statsd-jvm-profiler.jar=server=${INFLUX_HOST},port=${INFLUX_HTTP_PORT},reporter=InfluxDBReporter,database=${INFLUX_DATABASE_NAME},username=${INFLUX_USERNAME},password=${INFLUX_PASSWORD},prefix=sparkapp,tagMapping=spark" \
-    --conf spark.driver.extraJavaOptions=-javaagent:"/statsd-jvm-profiler.jar=server=${INFLUX_HOST},port=${INFLUX_HTTP_PORT},reporter=InfluxDBReporter,database=${INFLUX_DATABASE_NAME},username=${INFLUX_USERNAME},password=${INFLUX_PASSWORD},prefix=sparkapp,tagMapping=spark" \
-    --driver-java-options "-javaagent:/statsd-jvm-profiler.jar=server=${INFLUX_HOST},port=${INFLUX_HTTP_PORT},reporter=InfluxDBReporter,database=${INFLUX_DATABASE_NAME},username=${INFLUX_USERNAME},password=${INFLUX_PASSWORD},prefix=sparkapp,tagMapping=spark" \
+    --jars ${STATS_PROFILER_LOCATION}/statsd-jvm-profiler.jar \
+    --conf spark.executor.extraJavaOptions=-javaagent:"${STATS_PROFILER_LOCATION}/statsd-jvm-profiler.jar=server=${INFLUX_HOST},port=${INFLUX_HTTP_PORT},reporter=InfluxDBReporter,database=${INFLUX_DATABASE_NAME},username=${INFLUX_USERNAME},password=${INFLUX_PASSWORD},prefix=sparkapp,tagMapping=spark" \
+    --conf spark.driver.extraJavaOptions=-javaagent:"${STATS_PROFILER_LOCATION}/statsd-jvm-profiler.jar=server=${INFLUX_HOST},port=${INFLUX_HTTP_PORT},reporter=InfluxDBReporter,database=${INFLUX_DATABASE_NAME},username=${INFLUX_USERNAME},password=${INFLUX_PASSWORD},prefix=sparkapp,tagMapping=spark" \
+    --driver-java-options "-javaagent:${STATS_PROFILER_LOCATION}/statsd-jvm-profiler.jar=server=${INFLUX_HOST},port=${INFLUX_HTTP_PORT},reporter=InfluxDBReporter,database=${INFLUX_DATABASE_NAME},username=${INFLUX_USERNAME},password=${INFLUX_PASSWORD},prefix=sparkapp,tagMapping=spark" \
     --class ${SPARK_APPLICATION_MAIN_CLASS} \
     --master ${SPARK_MASTER_URL} \
+    ${SPARK_ADDITIONAL_CONF} \
     ${SPARK_APPLICATION_JAR_LOCATION} ${SPARK_APPLICATION_ARGS}
 
 python2.7 /influxdb_dump.py -o ${INFLUX_HOST} -r ${INFLUX_HTTP_PORT} -u ${INFLUX_USERNAME} -p ${INFLUX_PASSWORD} -d ${INFLUX_DATABASE_NAME} -t spark -e sparkapp -x /graphs/stack_traces
